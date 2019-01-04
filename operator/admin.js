691db9e7977e35b4ca81f66feefe917d00fec941
李湘华 <11407215@qq.com>
2019-01-05 03:30:22 +0800

    Update admin.js

--- operator/admin.js
+++ operator/admin.js
@@ -2,21 +2,6 @@ const models = require('../models/index');
 const adminModel = models.adminModel;
 
 
-/*const sha1 = require('sha1');
-let superAdmin = new adminModel({
-    name: 'admin',
-    password: sha1('lh526410'),
-    superAdmin:true
-});
-superAdmin.save().then(function(product) {
-console.log(product)
-});*/
-
-
 exports.checkUser = function (name) {
     return adminModel.findOne({name: name}).exec();
 };
-
-
-
-
